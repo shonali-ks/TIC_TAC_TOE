b0f17d5e7c6aa7fe97f175648b8c27ec5a6babab
shonali-ks <kshonalis72@gmail.com>
2020-07-01 17:58:55 +0530

    second

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path')
+
+const port = process.env.PORT || 3000;
+const app=express()
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
