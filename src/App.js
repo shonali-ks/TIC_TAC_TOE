e469818f27dc473c5d16902372375c41c5114150
sss9700 <50237152+sss9700@users.noreply.github.com>
2020-07-21 19:00:40 +0530

    Update App.js
    Imported rules here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react"
 import './App.css';
 import Firebase from './firebase/auth';
+import Rules from './rules'
 
 class App extends Component {
  
   render(){
     return (
     <div className="App">
-      <Firebase/>     
+      <Firebase/>  
+      <Rules/>
+
     </div>
     )
 }
